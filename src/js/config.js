a9d5816198fac37bf00976ff2d3bc5668730c9e3
moamen2020 <m01144944981@gmail.com>
2022-07-31 00:36:21 +0200

    Deploy Project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '03d4b2bf-9964-41c8-8bc5-6751417e954b';
+export const MODAL_CLOSE_SEC = 2.5;
